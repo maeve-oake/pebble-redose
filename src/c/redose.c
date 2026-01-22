026b4d8ca20f63ea044696446fbb49477e5dd0fc
maeve <maeve@oa.ke>
2026-01-22 13:34:29 +0000

    timeout app

--- src/c/redose.c
+++ src/c/redose.c
@@ -1,49 +1,44 @@
 #include <pebble.h>
 
-static Window *s_window;
-static TextLayer *s_text_layer;
+static Window* s_window;
+static TextLayer* s_text_layer;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
+static void prv_exit_application(void* data) {
+  exit_reason_set(APP_EXIT_ACTION_PERFORMED_SUCCESSFULLY);
+  // Exit the application by unloading the only window
+  window_stack_remove(s_window, false);
 }
 
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
+static void prv_exit_delay() {
+  int timeout = preferred_result_display_duration();
+  AppTimer* timer = app_timer_register(timeout, prv_exit_application, NULL);
 }
 
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
-}
-
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
-}
-
-static void prv_window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
+static void prv_window_load(Window* window) {
+  Layer* window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
+  s_text_layer = text_layer_create(GRect(0, (bounds.size.h / 2) - 30, bounds.size.w, 60));
+  text_layer_set_text(s_text_layer, "miaows at u");
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+
+  prv_exit_delay();
 }
 
-static void prv_window_unload(Window *window) {
+static void prv_window_unload(Window* window) {
   text_layer_destroy(s_text_layer);
 }
 
 static void prv_init(void) {
   s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
+
   window_set_window_handlers(s_window, (WindowHandlers) {
     .load = prv_window_load,
-    .unload = prv_window_unload,
+      .unload = prv_window_unload,
   });
-  const bool animated = true;
-  window_stack_push(s_window, animated);
+  window_stack_push(s_window, false);
 }
 
 static void prv_deinit(void) {
@@ -52,9 +47,6 @@ static void prv_deinit(void) {
 
 int main(void) {
   prv_init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
-
   app_event_loop();
   prv_deinit();
 }
